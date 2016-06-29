e2324ef364074fe0aa99b328ffb491d57a1a019b
ZhangGreatful <2450801771@qq.com>
2016-06-29 14:08:27 +0800

    为SplashActivity添加动画效果

--- app/src/main/java/com/example/administrator/gitdroid/splash/SplashPagerFragment.java
+++ app/src/main/java/com/example/administrator/gitdroid/splash/SplashPagerFragment.java
@@ -1,28 +1,41 @@
 package com.example.administrator.gitdroid.splash;
 
+import android.support.v4.app.Fragment;
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.example.administrator.gitdroid.R;
+import com.example.administrator.gitdroid.splash.pager.Pager2;
 
 import butterknife.Bind;
+import butterknife.BindColor;
 import butterknife.ButterKnife;
 import me.relex.circleindicator.CircleIndicator;
 
 /**
  * Created by Administrator on 2016/6/28 0028.
  */
-public class SplashPagerFragment extends android.support.v4.app.Fragment {
+public class SplashPagerFragment extends Fragment {
 
     //    绑定id
     @Bind(R.id.viewPager)
-    ViewPager       viewPager;
+    ViewPager viewPager;
     @Bind(R.id.indicator)
     CircleIndicator indicator;
+    @BindColor(R.color.colorGreen)
+    int colorGreen;
+    @BindColor(R.color.colorRed)
+    int colorRed;
+    @BindColor(R.color.colorYellow)
+    int colorYellow;
+    @Bind(R.id.content) FrameLayout framLayout;
+
     private SplashPagerAdapter adapter;
 
     @Nullable
@@ -38,7 +51,49 @@ public class SplashPagerFragment extends android.support.v4.app.Fragment {
         ButterKnife.bind(this, view);
         adapter = new SplashPagerAdapter(getContext());
         viewPager.setAdapter(adapter);
+//        给ViewPager设置监听
+        viewPager.addOnPageChangeListener(pagerChangeListener);
         indicator.setViewPager(viewPager);
 
     }
+
+    //      此监听器主要负责监听页面背景颜色的变化,和最后一个页面视图动画的显示
+    private final ViewPager.OnPageChangeListener pagerChangeListener = new ViewPager.OnPageChangeListener() {
+        //      ARGB取值器
+        final ArgbEvaluator evaluator = new ArgbEvaluator();
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//            第一个页面到第二个页面之间
+            if (position == 0) {
+//                第一个参数:    颜色绿色到红色的中间值[0-1]
+//                第二个参数:    初始颜色
+//                第三个参数:    结束颜色
+                int color = (int) evaluator.evaluate(positionOffset, colorGreen, colorRed);
+//                将颜色设置为FramLayout的背景颜色
+                framLayout.setBackgroundColor(color);
+                return;
+            }
+//            第二个页面到第三个页面之间
+            if (position == 1) {
+                int color = (int) evaluator.evaluate(positionOffset, colorRed, colorYellow);
+                framLayout.setBackgroundColor(color);
+                return;
+            }
+
+        }
+//      当进入第三张页面时,添加动画
+        @Override
+        public void onPageSelected(int position) {
+            if (position==2){
+                Pager2 pager2= (Pager2) adapter.getView(position);
+                pager2.showAnimation();
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
--- app/src/main/java/com/example/administrator/gitdroid/splash/pager/Pager2.java
+++ app/src/main/java/com/example/administrator/gitdroid/splash/pager/Pager2.java
@@ -4,13 +4,24 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.administrator.gitdroid.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2016/6/28 0028.
  */
 public class Pager2 extends FrameLayout {
+//    绑定视图id
+    @Bind(R.id.ivBubble1)
+    ImageView ivBubble1;
+    @Bind(R.id.ivBubble2) ImageView ivBubble2;
+    @Bind(R.id.ivBubble3) ImageView ivBubble3;
     public Pager2(Context context) {
         super(context);
         init();
@@ -28,5 +39,38 @@ public class Pager2 extends FrameLayout {
 
     public void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.content_pager_2, this, true);
+        ButterKnife.bind(this);
+        ivBubble1.setVisibility(GONE);
+        ivBubble2.setVisibility(GONE);
+        ivBubble3.setVisibility(GONE);
+
+    }
+//    显示当前页面的三个图像控件进入动画效果,只会显示一次
+    public void showAnimation(){
+        if(ivBubble1.getVisibility()!=VISIBLE){
+         postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ivBubble1.setVisibility(VISIBLE);
+                    YoYo.with(Techniques.FadeInLeft).duration(300).playOn(ivBubble1);
+                }
+            },50);
+            postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ivBubble2.setVisibility(VISIBLE);
+                    YoYo.with(Techniques.FadeInLeft).duration(300).playOn(ivBubble1);
+                }
+            },550);
+            postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ivBubble3.setVisibility(VISIBLE);
+                    YoYo.with(Techniques.FadeInLeft).duration(300).playOn(ivBubble1);
+                }
+            },1050);
+        }
+
+
     }
 }
